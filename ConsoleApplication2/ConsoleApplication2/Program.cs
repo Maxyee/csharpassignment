8f2ad17eb1d9c8c37d1f39c239fe79a4fd376d05
MD. JULHAS HOSSAIN (MOHON) <eyaminkhan00@gmail.com>
2017-01-09 21:32:22 +0600

    Add files via upload

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int A = Convert.ToInt32(Console.ReadLine());
+            int B = Convert.ToInt32(Console.ReadLine());
+            int SOMA = A + B;
+            Console.WriteLine("SOMA = "+SOMA+"\n");
+            Console.ReadKey();
+        }
+    }
+}
