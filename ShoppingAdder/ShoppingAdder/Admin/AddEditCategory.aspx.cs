53780a4fe094e67defc424268efb817a3bad6358
MD. JULHAS HOSSAIN (MOHON) <eyaminkhan00@gmail.com>
2017-01-13 15:27:42 +0600

    Add files via upload

---
+++ ShoppingAdder/ShoppingAdder/Admin/AddEditCategory.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ShoppingAdder.Admin
+{
+    public partial class AddEditCategory : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            ShoppingAdder k = new ShoppingAdder
+            {
+                CategoryName = txtCategoryName.Text
+            };
+            k.AddNewCategory();
+            txtCategoryName.Text = string.Empty;
+            Response.Redirect("~/Admin/AddNewProducts.aspx");
+        }
+    }
+}
